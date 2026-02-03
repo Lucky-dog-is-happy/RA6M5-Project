108ae6cf511ccaa7b093ec11cc92f78570d3379e
lucky <1210833823@qq.com>
2026-02-03 20:59:57 +0800

    Rename from UART to RA6M5-Project

--- build/Debug/CMakeFiles/4.2.3/CMakeSystem.cmake
+++ build/Debug/CMakeFiles/4.2.3/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Linux")
 set(CMAKE_HOST_SYSTEM_VERSION "6.18.7-arch1-1")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/home/lucky/repo/RA6M5/UART/cmake/gcc.cmake")
+include("/home/lucky/repo/RA6M5/RA6M5-Project/cmake/gcc.cmake")
 
 set(CMAKE_SYSTEM "Generic")
 set(CMAKE_SYSTEM_NAME "Generic")
